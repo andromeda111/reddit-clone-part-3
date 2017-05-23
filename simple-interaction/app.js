b6edf9504269155e231b364455b4e1afc4067fb7
John <jmseymour111@gmail.com>
2017-05-23 14:27:48 -0700

    simple app done

--- simple-interaction/app.js
+++ simple-interaction/app.js
@@ -1,4 +1,41 @@
 (function() {
   'use strict'
+  console.log('ready');
+
+  angular.module("app", [])
+        .component('name', {
+          controller: function () {
+            const vm = this
+
+            vm.$onInit = function () {
+              vm.firstName = ''
+              vm.nickname = ''
+              vm.lastName = ''
+              vm.fullName = ''
+            }
+            vm.submit = function () {
+              if (vm.nickname !== '') {
+                vm.nickname = `"${vm.nickname}"`
+                vm.fullName = vm.firstName + ' ' + vm.nickname + ' ' + vm.lastName
+              } else {
+                vm.fullName = vm.firstName + ' ' + vm.lastName 
+              }
+            }
+
+          },
+
+          template: `
+
+
+            <p><input ng-model="$ctrl.firstName" placeholder="First Name"/></p>
+            <p><input ng-model="$ctrl.nickname" placeholder="Nickname"/></p>
+            <p><input ng-model="$ctrl.lastName" placeholder="Last Name"/></p>
+            <p><button ng-click="$ctrl.submit()">Submit</button></p>
+
+
+            <p>{{$ctrl.fullName}}</p>
+
+          `
+        })
 
 }());
