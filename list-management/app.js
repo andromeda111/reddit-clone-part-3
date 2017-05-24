3333d0edd64d9d755a8b1e1934b51eb2e426d1e6
John <jmseymour111@gmail.com>
2017-05-23 17:00:10 -0700

    Done

--- list-management/app.js
+++ list-management/app.js
@@ -2,6 +2,49 @@
   'use strict'
 
   angular.module('app', [])
-    .component('cart', {})
+    .component('cart', {
+      controller: function () {
+        const vm = this
+
+        vm.$onInit = function () {
+          vm.items = [{name: "some name", quantity: 4}]
+        }
+
+        vm.setItems = function () {
+          vm.items.push(vm.item)
+          delete vm.item
+        }
+        vm.addItem = function () {
+          vm.setItems()
+        }
+
+        vm.deleteItem = function (e, item) {
+          e.preventDefault()
+          vm.items.splice(vm.items.indexOf(item), 1)
+        }
+      },
+
+      template: `
+          <form ng-submit="$ctrl.addItem()">
+            <p>
+              <label for="name">Name</label>
+              <input ng-model="$ctrl.item.name" id="name">
+            </p>
+            <p>
+              <label for="quantity">Quantity</label>
+              <input ng-model="$ctrl.item.quantity" id="quantity">
+            </p>
+            <p>
+              <button type="submit">Add Item</button>
+            </p>
+          </form>
+
+          <div class="item" ng-repeat="item in $ctrl.items">
+            {{item.name}} {{item.quantity}}
+            <a href="#" ng-click="$ctrl.deleteItem($event, item)">Delete</a>
+          </div>
+      `
+    })
+
 
 }());
