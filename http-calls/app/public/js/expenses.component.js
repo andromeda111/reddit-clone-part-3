21d32e7f7fc0da1c71822d9bb2119a565125f79a
John <jmseymour111@gmail.com>
2017-05-28 14:46:25 -0700

    done with http calls

--- http-calls/app/public/js/expenses.component.js
+++ http-calls/app/public/js/expenses.component.js
@@ -14,17 +14,53 @@
     vm.$onInit = function () {
       $http.get('/api/expenses').then(result => {
         vm.expenseList = result.data
+        vm.editToggle = false
       })
     }
 
     vm.addExpense = function () {
-      vm.expense.amount = vm.expense.amount.toFixed(2)
-      var newData = {category: vm.expense.category, amount: vm.expense.amount}
-      $http.post('/api/expenses', newData).then(result => {
+      console.log(vm.expense); // Logging because certain numbers come in undefined, can't figure out why after 1hr research & experimentation...
+      $http.post('/api/expenses', vm.expense).then(result => {
         vm.expenseList.push(result.data);
       })
       delete vm.expense
     }
+
+    vm.showEdit = function (data) {
+      vm.selectedData = {id: data.id, category: data.category, amount: data.amount}
+      vm.selectedData.amount = Number(vm.selectedData.amount)
+      if (vm.editToggle) {
+        vm.editToggle = false
+      } else {
+        vm.editToggle = true
+      }
+    }
+
+    vm.editExpense = function (data) {
+      vm.editData = data
+
+      if (vm.editToggle) {
+        vm.editToggle = false
+      } else {
+        vm.editToggle = true
+      }
+
+      $http.patch(`/api/expenses/${vm.editData.id}`, vm.editData).then(() => {
+        $http.get('/api/expenses').then(result => {
+          vm.expenseList = result.data
+        })
+      })
+    }
+
+    vm.deleteExpense = function (data) {
+      vm.data = data
+      $http.delete(`/api/expenses/${vm.data.id}`).then(() => {
+        $http.get('/api/expenses').then(result => {
+          vm.expenseList = result.data
+        })
+      })
+    }
   }
+
 
 }());
